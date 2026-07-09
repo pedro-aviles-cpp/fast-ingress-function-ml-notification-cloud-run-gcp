1d42af16e70de1a45e7788b32efc5aee251e9a7e
pedro <pedro.aviles.cpp@outlook.com>
2026-07-08 19:59:47 -0600

    added logic for env variables and white list

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ const functions = require('@google-cloud/functions-framework');
 // Register an HTTP function named "fast-ingress-function"
 functions.http('fast-ingress-function', async (req, res) => {
 
-    console.log('=========== Starting Fast Ingress ===========');
+  console.log('=========== Starting Fast Ingress ===========');
+
+
+    // 1. Fetch the raw string from environment variables
+  const rawIpString = process.env.TRUSTED_IP_WHITELIST || "";
+
+  // 2. Convert it back into a Set safely
+  const trustedIpWhitelist = new Set(
+    rawIpString ? rawIpString.split(',').map(ip => ip.trim()) : []
+  );
+
+  // 1. Whitelist validation
+  const isAllowed = trustedIpWhitelist.has(req.ip);
+
+  if (!isAllowed) {
+      console.log(`IP Whitelist Filter IP BLOCKED: ${req.ip} body:${JSON.stringify(req.body)}`);
+      return res.status(200).send('Ok'); 
+  }
 
     // // 1. Decode and parse the Pub/Sub payload
     // const base64Data = cloudEvent.data.message.data;
@@ -17,7 +34,7 @@ functions.http('fast-ingress-function', async (req, res) => {
     // // Run both tasks simultaneously
     // await Promise.allSettled([newRelicTask]);//, vpsTask]);
 
-    console.log('=========== End Fast Ingress ===========');
+  console.log('=========== End Fast Ingress ===========');
 
 
   res.status(200).send('Hello, World! prod fast ingress function');
